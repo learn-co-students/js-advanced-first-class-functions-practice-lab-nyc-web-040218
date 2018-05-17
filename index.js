122b695b93e98e44315de4df81cb5a071214b38c
briecodes <hello@brie.work>
2018-05-16 20:57:21 -0400

    functions partially defined.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+    for (const driver of drivers){
+        console.log(driver.name);
+    }
+}
+
+function logDriversByHometown(drivers, location) {
+    for (const driver of drivers){
+        if (driver.hometown === location){
+            console.log(driver.name);
+        }
+    }
+}
+
+function driversByRevenue(drivers) {
+    let newArray = drivers;
+    newArray.sort(function (a,b){
+        return a["revenue"] - b["revenue"];
+    });
+    return newArray;
+}
+
+function totalRevenue(drivers) {
+    
+}
