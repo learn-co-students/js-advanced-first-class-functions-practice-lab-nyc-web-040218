f91293358ec8240715f62fd81171a84fecff23a9
briecodes <hello@brie.work>
2018-05-17 09:09:12 -0400

    lab completed.

--- index.js
+++ index.js
@@ -13,13 +13,35 @@ function logDriversByHometown(drivers, location) {
 }
 
 function driversByRevenue(drivers) {
-    let newArray = drivers;
+    let newArray = [...drivers];
     newArray.sort(function (a,b){
-        return a["revenue"] - b["revenue"];
+        return a.revenue - b.revenue;
+    });
+    return newArray;
+}
+
+function driversByName(drivers) {
+    const newArray = [...drivers];
+    newArray.sort(function (a,b){
+        return a.name.localeCompare(b.name);
     });
     return newArray;
 }
 
 function totalRevenue(drivers) {
-    
+    let total = 0;
+    drivers.forEach(function(driver){
+        total += driver.revenue;
+    });
+    return total;
+}
+
+function averageRevenue(drivers) {
+    let total = 0;
+    let i = 0;
+    drivers.forEach(function(driver){
+        ++i
+        total += driver.revenue;
+    });
+    return total/i;
 }
